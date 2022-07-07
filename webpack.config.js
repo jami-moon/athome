7fecbdcfb944fc208f736e7275ecc45afd7aeb27
kyukyu0312 <kyukyu0312@adnstyle.com>
2022-07-07 11:09:24 +0900

    Wip: 개발환경 세팅 중

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    context: __dirname,
-    entry: './src/js/index.js',
-    output: {
-        publicPath: '/dist/',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
-}
+	context: __dirname,
+	entry: "./src/scss/main.scss",
+	output: {
+		path: path.resolve(__dirname, "public"),
+	},
+	plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+				exclude: /node_modules/,
+			},
+		],
+	},
+    mode: 'production'
+};
