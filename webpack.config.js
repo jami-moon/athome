d2483e294422d10ccaf7d443c446ba8eaa4ecdad
kyukyu0312 <kyukyu0312@adnstyle.com>
2022-08-24 17:54:11 +0900

    feat: 환경 셋팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StylelintWebpackPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
 	context: __dirname,
-	entry: "./src/scss/main.scss",
+	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, "public"),
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
 	},
-	plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
 	module: {
 		rules: [
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.s[ac]ss$/i, 
 				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
 				exclude: /node_modules/,
 			},
 		],
 	},
+	plugins: [
+		new CleanWebpackPlugin (),
+		new HtmlWebpackPlugin({
+			template: "./src/html/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "./style.css",
+		}),
+		new StylelintWebpackPlugin({
+			configFile: ".stylelintrc",
+			files: "./**.*.scss",
+			fix: true,
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "dist"),
+		},
+		port: 8020,
+	},
+	devtool: 'source-map',
     mode: 'production'
 };
